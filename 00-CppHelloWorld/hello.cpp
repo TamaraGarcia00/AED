69c1f6849676f192ea02ed8fc03577f8f7843126
TamaraGarcia00 <tagarcia@frba.edu.com.ar>
2026-04-25 21:24:01 -0300

    TP0

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+int main(){
+    std::cout << "Hello, World!\n";
+
+    std::string nombre = "Tamara Garcia\n";
+    std::cout << nombre;
+
+    std::string curso = "K1053\n";
+    std::cout << curso;
+
+    int grupo = 8;   
+    std::cout << grupo;
+}
